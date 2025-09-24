5b6cf8f3c0d7a50d0a61348e3f0c5d108d771d69
SindreEieLedsaak <sindre.eie@gmail.com>
2025-09-24 14:23:44 +0200

    added the build frontend file to the backend

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+app.use(express.static('dist'))
 
 
 morgan.token('body', (request) => {
@@ -118,4 +114,10 @@ const errorHandler = (error, request, response, next) => {
 }
 
 app.use(errorHandler)
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
 
